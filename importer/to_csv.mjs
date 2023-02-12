25e33c1e1d933ffadff9f929e6f0889bf3480ac9
Arthur Sonzogni <sonzogniarthur@gmail.com>
2023-02-12 22:23:47 +0100

    Add more social graph

--- importer/to_csv.mjs
+++ importer/to_csv.mjs
@@ -1,12 +1,12 @@
 import * as filesystem from "fs";
 const fs = filesystem.promises;
 
-const user_file = "data/users.json";
-const user_dir = "data/users";
+const user_file = "../static/data/users.json";
+const user_dir = "../static/data/users";
 
 const users = JSON.parse(await fs.readFile(user_file, "utf8"));
-const date_after = new Date("2000-01-01");
-const date_before = new Date("2025-01-01");
+const date_after = new Date("2022-01-01");
+const date_before = new Date("2023-01-01");
 for(const user of users) {
   const data = JSON.parse(await fs.readFile(`${user_dir}/${user}.json`, "utf8"));
   for(const time in data.author) {
@@ -16,10 +16,4 @@ for(const user of users) {
       }
     }
   }
-  for(const time in data.review) {
-    if (new Date(time) > date_after && new Date(time) < date_before) {
-      console.log(`${data.review[time]},${user}`);
-    }
-  }
 }
-
