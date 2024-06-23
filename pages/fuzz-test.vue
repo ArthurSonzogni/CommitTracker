8489ca58753154612844d0ed9f7b22cde5153f82
ArthurSonzogni <sonzogniarthur@gmail.com>
2024-06-23 13:30:20 +0200

    Improve /fuzz-tests

--- pages/fuzz-test.vue
+++ pages/fuzz-test.vue
@@ -4,50 +4,69 @@
 
     <section class="section">
       <div class="container">
-        <h1 class="title">{{data.length}} FUZZ_TEST() in chromium</h1>
+        <h1 class="title">
+          <b-icon icon="strike" />
+          {{data.length}}
+        fuzzers in chromium</h1>
 
         <LineChart :data="graph" />
+      </div>
+    </section>
 
-        <div align="center">
-          <b-table
-            :data="data"
-            scrollable
-            striped
-            hoverable
-            >
-            <b-table-column field="date" label="Date" v-slot="props" sortable >
-              {{ props.row.date }}
-            </b-table-column>
+    <section class="section">
+      <div class="container">
+        <h2 class="title">Fuzzers</h2>
+        <b-table
+          :data="data"
+          hoverable
+          scrollable
+          height="500"
+          sticky-header
+          striped
+          >
+          <b-table-column field="date" label="Date" v-slot="props" sortable >
+            {{ props.row.date }}
+          </b-table-column>
 
-            <b-table-column field="author" label="Author" v-slot="props">
+          <b-table-column field="author" label="Author" v-slot="props" sortable>
+            <router-link :to="{
+            name: 'individuals',
+            query: {
+            developers: props.row.author,
+            repositories: 'chromium',
+            }
+            }">
               {{ props.row.author }}
-            </b-table-column>
+            </router-link>
+          </b-table-column>
 
-            <b-table-column field="suite_name" label="Suite Name" v-slot="props">
+          <b-table-column field="suite_name" label="Name" v-slot="props" sortable>
+            <a :href="'https://cs.chromium.org/chromium/src/' + props.row.file
+            + '?l=' + props.row.line
+            + ';q=' + props.row.function
+            "
+              >
               {{ props.row.suite_name }}
-            </b-table-column>
+            </a>
+          </b-table-column>
 
-            <b-table-column field="function" label="Function" v-slot="props">
-              {{ props.row.function }}
-            </b-table-column>
+          <b-table-column field="sha" label="Patch" v-slot="props">
+            <a :href="'https://chromium-review.googlesource.com/q/' + props.row.sha">
+              Open
+            </a>
+          </b-table-column>
 
-            <b-table-column field="link" label="Code" v-slot="props">
-              <a :href="'https://cs.chromium.org/chromium/src/' + props.row.file
-                + '?l=' + props.row.line
-                + ';q=' + props.row.function
-                "
-               >
-                Open
-              </a>
-            </b-table-column>
+          <b-table-column field="type" label="Type" v-slot="props" sortable>
+            <b-tag type="is-info" v-if="props.row.type == 'fuzzer_test'">
+              fuzz target
+            </b-tag>
+            <b-tag type="is-danger" v-else-if="props.row.type == 'FUZZ_TEST'">
+              FUZZ_TEST
+            </b-tag>
+            <b-tag type="is-danger" v-else>{{ props.row.type }}</b-tag>
+          </b-table-column>
 
-            <b-table-column field="patch" label="Patch" v-slot="props">
-              <a :href="'https://chromium-review.googlesource.com/q/' + props.row.sha">
-                Open
-              </a>
-            </b-table-column>
-          </b-table>
-        </div>
+        </b-table>
       </div>
     </section>
 
@@ -56,18 +75,37 @@
         <h2 class="title">Leaderboard</h2>
         <b-table
           :data="leaderboard"
-          scrollable
-          striped
           hoverable
+          striped
           >
-          <b-table-column field="author" label="Author" v-slot="props">
-            {{ props.row.icon }}
-            {{ props.row.author }}
-          </b-table-column>
 
-          <b-table-column field="count" label="Fuzz tests" v-slot="props">
+          <b-table-column field="count" label="Fuzzers" v-slot="props">
             {{ props.row.count }}
           </b-table-column>
+
+          <b-table-column
+            field="author"
+            label="Authors"
+            v-slot="props"
+          >
+            <div class="leaderboard-authors">
+              <router-link
+                v-for="author in props.row.authors"
+                class="leaderboard-author"
+                :to="{
+                  name: 'individuals',
+                  query: {
+                    developers: author,
+                    repositories: 'chromium',
+                  }
+                }">
+                <span v-if="props.row.icon">{{ props.row.icon }}</span>
+                {{ author }}
+              </router-link>
+              <div class="flex-end"></div>
+            </div>
+          </b-table-column>
+
         </b-table>
       </div>
     </section>
@@ -127,7 +165,9 @@ const getData = async function() {
     d.author = d.author.split('@')[0];
   });
 
+  await new Promise(r => setTimeout(r, 1));
   data.value = response_json;
+  await new Promise(r => setTimeout(r, 1));
 
   // Compute the "graph" value. Format should be:
   // [
@@ -152,9 +192,10 @@ const getData = async function() {
   graph.value = [
     {
       label: "FUZZ_TEST",
-      values: new_values,
+      values: structuredClone(new_values),
     },
   ];
+  response_json.reverse();
 
   // Compute the leaderboard
   const author_count = {}
@@ -163,29 +204,36 @@ const getData = async function() {
     author_count[d.author]++;
   }
 
-  const leaderboard_value =
-    Object.entries(author_count)
-    .map(([author, count]) => ({ author, count }))
-    .sort((a, b) => b.count - a.count)
+  // Invert the map:
+  const count_author = {}
+  for(const [author, count] of Object.entries(author_count)) {
+    count_author[count] ||= [];
+    count_author[count].push(author);
+  }
 
-  // Define an icon for the best author
-  for(let i=0; i<leaderboard_value.length; i++) {
-    switch(i) {
-      case 0:
-        leaderboard_value[i].icon = '🥇';
-        break;
-      case 1:
-        leaderboard_value[i].icon = '🥈';
-        break;
-      case 2:
-        leaderboard_value[i].icon = '🥉';
-        break;
-      default:
-        leaderboard_value[i].icon = ' ';
+  const count_author_array = Object
+    .entries(count_author)
+    .map(([count, authors]) => {
+      return {count, authors}
+    })
+    .sort((a, b) => b.count - a.count);
 
+
+  // Add icon to each groups.
+  const add_icon = (i, icon) => {
+    if (count_author_array[i]) {
+      count_author_array[i].icon = icon;
     }
   }
-  leaderboard.value = leaderboard_value;
+
+  add_icon(0, '🥇');
+  add_icon(1, '🥈');
+  add_icon(2, '🥉');
+
+
+  await new Promise(r => setTimeout(r, 1));
+  leaderboard.value = count_author_array;
+  await new Promise(r => setTimeout(r, 1));
 };
 
 onMounted(() => {
@@ -233,6 +281,38 @@ watch(dates, () => {
   bottom: 0;
   margin-bottom:0px;
   padding-bottom:15px;
+}
+
+.leaderboard-authors {
+  display:flex;
+  flex-direction:row;
+  flex-wrap:wrap;
+}
+
+.leaderboard-author {
+  margin:2px;
+  padding:10px;
+  flex-grow:1;
+  background-color: rgba(0,0,0,0.1);
+  color:black;
+  border-radius: 15px;
+
+  a {
+    color:black;
+    padding:auto;
+  }
+}
+
+.leaderboard-author:hover {
+  background-color: rgba(0,0,0,0.2);
+}
+
+.leaderboard-author:active{
+  background-color: rgba(0,0,0,0.3);
+}
+
+.flex-end {
+  flex-grow:1000;
 }
 
 </style>
