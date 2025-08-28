77de444b9612d863914cbe5ce498f06df4b4d96d
ArthurSonzogni <sonzogniarthur@gmail.com>
2025-08-28 08:41:13 +0200

    Fix importer local key retrieval.

--- importer/octokit.mjs
+++ importer/octokit.mjs
@@ -8,7 +8,13 @@ const MyOctokit = Octokit.plugin(throttling);
 const AuthToken = async () => {
   try {
     const token = await fs.readFile('.token', 'utf8');
-    return
+    if (token.length != 41) {
+      console.error("Token in .token file is not 40 characters long");
+      throw new Error("Invalid token length");
+    }
+
+    console.log("Using token from .token file");
+    return token;
   } catch (e) {}
 
   if (process.env.GITHUB_TOKEN) {
