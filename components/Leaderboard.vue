ArthurSonzogni/CommitTracker

commit 63c6a24356d9a8d197f8b63286f3365857a8f5f1

Fix `container.value.clientWidth` null deref.
This potentially fix bug:
https://github.com/ArthurSonzogni/CommitTracker/issues/24

I think the error happened during `onMounted` where the `container` ref
hasn't been fully rendered and bound to the DOM element yet.

Bug:https://github.com/ArthurSonzogni/CommitTracker/issues/24

components/Leaderboard.vue
@@ -34,9 +34,9 @@
       </b-field>
     </b-field>
 
-    <svg ref="svg_axis" :width="svgWidth" :height="31" class="sticky-top"></svg>
-    <svg ref="svg" :width="svgWidth" :height="svgHeight"></svg>
-    <svg ref="svg_title" :width="svgWidth" :height="80" class="sticky-bottom"> </svg>
+    <svg ref="svg_axis" width="100%" :height="31" class="sticky-top"></svg>
+    <svg ref="svg" width="100%" :height="svgHeight"></svg>
+    <svg ref="svg_title" width="100%" :height="80" class="sticky-bottom"> </svg>
   </div>
 </template>
 
@@ -71,12 +71,13 @@ const svg_axis = ref(null);
 const svg_title = ref(null);
 
 const take_n = 100;
+const svgHeight = take_n * 50;
 
 const animateWithAccu = ref<string>('instant');
-const svgWidth = ref<number>(300);
-const svgHeight = ref<number>(300);
 const date = ref<string[]>([]);
 const is_animating = ref<boolean>(false);
+
+const svgWidth = () => container.value?.clientWidth || 800;
 
 const timeLabel = computed(() => {
   switch(props.grouping) {
@@ -155,7 +156,7 @@ const axis = (svg) => {
 
   return (x, transition) => {
     const axis = axisTop(x)
-      .ticks(svgWidth.value / 100)
+      .ticks(svgWidth() / 100)
       .tickSizeOuter(0)
       .tickSizeInner(-svgHeight.value + 100)
       .tickFormat(d => format(".2s")(d).replace("G", "B"))
@@ -175,7 +176,7 @@ const axisSticky = (svg) => {
 
   return (x, transition) => {
     const axis = axisTop(x)
-      .ticks(svgWidth.value / 100)
+      .ticks(svgWidth() / 100)
 
     g.transition(transition).call(axis)
   };
@@ -421,8 +422,6 @@ const next = () => {
   timeIndexModel.value = Math.max(timeIndexModel.value - 1, 0);
 };
 
-watch(props, fetchData)
-
 let updateBars = null;
 let updateAxis = null;
 let updateAxisSticky = null;
@@ -434,7 +433,7 @@ const render = (frame, transition) => {
 
   const title_update = title => {
     return title
-      .attr("x", svgWidth.value - 10)
+      .attr("x", svgWidth() - 10)
       .attr("y", 80-32)
       .transition(transition)
       .text(d => d)
@@ -477,7 +476,7 @@ const render = (frame, transition) => {
 
   const x = scaleLinear()
     .domain([0, max_commit])
-    .range([25, svgWidth.value - 10]);
+    .range([25, svgWidth() - 10]);
 
   const y = scaleBand()
     .domain(range(take_n+1))
@@ -489,21 +488,14 @@ const render = (frame, transition) => {
   updateAxisSticky(x, transition);
 };
 
-const onResize = () => {
-  if (container.value) {
-    svgWidth.value = container.value.clientWidth;
-    svgHeight.value = take_n * 50;
-    fetchData();
-  }
-};
-window.addEventListener("resize", onResize);
+window.addEventListener("resize", fetchData);
+watch(props, fetchData)
 
 onMounted(() => {
   updateBars = bars(select(svg.value));
   updateAxis = axis(select(svg.value));
   updateAxisSticky = axisSticky(select(svg_axis.value))
-
-  onResize();
+  fetchData();
 });
 
 

components/PeersChart.vue
@@ -1,6 +1,6 @@
 <template>
   <div ref="container" align="center">
-    <svg ref="svg" :width="svgWidth" :height="svgHeight">
+    <svg ref="svg" width="100%" :height="svgHeight">
       <g ref="axis"></g>
       <g ref="authors"></g>
     </svg>
@@ -45,12 +45,11 @@ interface Data {
 }
 
 const data = ref<Array<Data>>([]);
-const svgWidth = ref<number>(300);
-const svgHeight = ref<number>(300);
+const svgHeight = computed(() => props.take_n * 20 + 50);
 
-watch(() => [props.developers, props.repositories], async () => {
+const fetchData = async () => {
   data.value = await $chromiumDataAll(props.repositories[0], props.developers);
-});
+}
 
 const filteredData = computed(() => {
   // Filter and count peers for every developers.
@@ -136,9 +135,11 @@ const filteredData = computed(() => {
 });
 
 const render = () => {
+  const svgWidth = container.value?.clientWidth || 800;
+
   const scale = scaleLinear()
     .domain([0, max(filteredData.value, d => max(d.developers, d => d.right))])
-    .range([0, svgWidth.value - 200]);
+    .range([0, svgWidth - 200]);
 
   const axis_ = axisTop(scale)
 
@@ -261,17 +262,10 @@ const render = () => {
       },
     )
 }
+watch(() => [props.developers, props.repositories], fetchData);
+window.addEventListener("resize", render)
+onMounted(render);
 watch(filteredData, render);
-
-const initialize = async () => {
-  svgHeight.value = props.take_n * 20 + 50;
-  svgWidth.value = container.value.clientWidth;
-  render();
-  window.addEventListener("resize", initialize);
-}
-
-onMounted(() => {
-  initialize();
-});
+fetchData();
 
 </script>

components/TimelineWrappedChart.vue
@@ -1,6 +1,6 @@
 <template>
   <div ref="container" align="center">
-    <svg ref="svg" :width="svgWidth" :height="svgHeight">
+    <svg ref="svg" :width="svgWidth" :height="svgWidth">
       <g ref="arcs"></g>
       <g ref="indicators"></g>
       <g ref="legend"></g>
@@ -60,7 +60,6 @@ interface Data {
 
 const data = ref<Data>([]);
 const svgWidth = ref<number>(300);
-const svgHeight = ref<number>(300);
 
 const modulo = computed(() => {
   switch(props.hourlyParam) {
@@ -149,12 +148,11 @@ const developersChanged = async () => {
 
 const initialize = () => {
   try {
-    svgWidth.value = container.value.clientWidth;
+    svgWidth.value = container.value?.clientWidth || 300;
     svgWidth.value = Math.min(svgWidth.value, 800);
     svgWidth.value = Math.min(svgWidth.value, window.innerWidth * 0.8);
     svgWidth.value = Math.min(svgWidth.value,
       Math.max(0, (window.innerHeight - 300) * 0.9));
-    svgHeight.value = svgWidth.value;
   } catch (e) {
     console.log(e);
   }
@@ -165,12 +163,12 @@ const initialize = () => {
 
 const render = () => {
   select(arcs.value)
-    .attr("transform", `translate(${svgWidth.value/2}, ${svgHeight.value/2})`);
+    .attr("transform", `translate(${svgWidth.value/2}, ${svgWidth.value/2})`);
   select(indicators.value)
-    .attr("transform", `translate(${svgWidth.value/2}, ${svgHeight.value/2})`);
+    .attr("transform", `translate(${svgWidth.value/2}, ${svgWidth.value/2})`);
 
-  const innerRadius = Math.max(svgWidth.value, svgHeight.value) * 0.2;
-  const outerRadius = Math.max(svgWidth.value, svgHeight.value) * 0.5;
+  const innerRadius = svgWidth.value * 0.2;
+  const outerRadius = svgWidth.value * 0.5;
 
   const x = scaleLinear()
     .domain([0, props.buckets])

commit 56425e2de3d68f3c0ee24490dd32da551fd3a182

Fix /leaderboard invalid svgHeight.
A bug was introduced by:
https://github.com/ArthurSonzogni/CommitTracker/commit/63c6a24356d9a8d197f8b63286f3365857a8f5f1#diff-7b047cbb6b919e51df8ac78d7e35cb6ca5571523e65e58ebfccc7701d27939ff

The svgHeight is now a constant. It isn't dynamic anymore.

components/Leaderboard.vue
@@ -158,7 +158,7 @@ const axis = (svg) => {
     const axis = axisTop(x)
       .ticks(svgWidth() / 100)
       .tickSizeOuter(0)
-      .tickSizeInner(-svgHeight.value + 100)
+      .tickSizeInner(-svgHeight + 100)
       .tickFormat(d => format(".2s")(d).replace("G", "B"))
 
     g.transition(transition).call(axis)
@@ -480,7 +480,7 @@ const render = (frame, transition) => {
 
   const y = scaleBand()
     .domain(range(take_n+1))
-    .range([0, svgHeight.value - 60])
+    .range([0, svgHeight - 60])
     .padding(0.1);
 
   updateBars(x, y, frame, transition);